f588bef4fb36a89b0cba0d84d88bdb2734417eb4
jstonedev <jstonedev521@gmail.com>
2020-12-04 22:47:44 -0500

    create & style popular component w/ nav links

---
+++ app/components/Popular.js
@@ -0,0 +1,17 @@
+import React, { Component } from "react";
+
+export default class Popular extends Component {
+	render() {
+		const languages = ["All", "JavaScript", "Ruby", "Java", "CSS", "Python"];
+
+		return (
+			<ul className="flex-center">
+				{languages.map((language, i) => (
+					<li key={language}>
+						<button className="btn-clear nav-link">{language}</button>
+					</li>
+				))}
+			</ul>
+		);
+	}
+}
--- app/index.js
+++ app/index.js
@@ -1,10 +1,15 @@
 import React, { Component } from "react";
 import ReactDOM from "react-dom";
+import Popular from "./components/Popular";
 import "./style.css";
 
 class App extends Component {
 	render() {
-		return <div>Hello, World!</div>;
+		return (
+			<div className="container">
+				<Popular />
+			</div>
+		);
 	}
 }
 
