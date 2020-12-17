03b035f83b1b1cf5b5a018d5186ea72c95081ce3
jstonedev <jstonedev521@gmail.com>
2020-12-16 20:54:46 -0500

    setup repo fetching

--- app/components/Popular.js
+++ app/components/Popular.js
@@ -1,17 +1,46 @@
-import React, { Component } from "react";
+import React, { Fragment, useEffect, useState } from "react";
+import LanguageNav from "./LanguageNav";
+import { fetchPopularRepos } from "../utils/api";
 
-export default class Popular extends Component {
-	render() {
-		const languages = ["All", "JavaScript", "Ruby", "Java", "CSS", "Python"];
+export default function Popular() {
+	const [selectedLanguage, setSelectedLanguage] = useState("All");
+	const [repos, setRepos] = useState(null);
+	const [error, setError] = useState(null);
 
-		return (
-			<ul className="flex-center">
-				{languages.map((language, i) => (
-					<li key={language}>
-						<button className="btn-clear nav-link">{language}</button>
-					</li>
-				))}
-			</ul>
-		);
-	}
+	useEffect(() => {
+		updateLanguage(selectedLanguage);
+	}, []);
+
+	const updateLanguage = (languageSelected) => {
+		setSelectedLanguage(languageSelected);
+		setRepos(null);
+		setError(null);
+
+		fetchPopularRepos(languageSelected)
+			.then((repos) => setRepos(repos))
+			.then(() => setError(null))
+			.catch(() => {
+				console.warn("Error fetching repos: ", error);
+				setError("There was an error fetching the repositiories!");
+			});
+	};
+
+	const isLoading = () => {
+		return repos === null && error === null;
+	};
+
+	return (
+		<Fragment>
+			<LanguageNav
+				selected={selectedLanguage}
+				onUpdateLanguage={updateLanguage}
+			/>
+
+			{isLoading() && <p>Loading...</p>}
+
+			{error && <p>{error}</p>}
+
+			{repos && <pre>{JSON.stringify(repos, null, 2)}</pre>}
+		</Fragment>
+	);
 }
