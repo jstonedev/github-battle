68f7a7724befb7405cb07d868fb6e341ca0c2b97
jstonedev <jstonedev521@gmail.com>
2020-12-17 11:29:10 -0500

    update api component with battle functionality. log users to console

--- app/components/Results.js
+++ app/components/Results.js
@@ -1,10 +1,17 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { battle } from "../utils/api";
 
-function Results(props) {
+function Results({ playerOne, playerTwo }) {
+	useEffect(() => {
+		battle([playerOne, playerTwo]).then((players) => {
+			console.log("data: ", players);
+		});
+	});
+
 	return (
 		<div>
 			Results
-			<pre>{JSON.stringify(props, null, 2)}</pre>
+			{/* <pre>{JSON.stringify(props, null, 2)}</pre> */}
 		</div>
 	);
 }
