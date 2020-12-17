d12ed3dfcf2efbd711a73021bc483f7b1c5f7d10
jstonedev <jstonedev521@gmail.com>
2020-12-17 02:08:02 -0500

    create & style player preview component

--- app/components/Battle.js
+++ app/components/Battle.js
@@ -1,6 +1,7 @@
 import React, { Fragment, useState } from "react";
 import Instructions from "./Instructions";
 import PlayerInput from "./PlayerInput";
+import PlayerPreview from "./PlayerPreview";
 
 function Battle() {
 	const [playerOne, setPlayerOne] = useState(null);
@@ -13,16 +14,28 @@ function Battle() {
 			<div className="players-container">
 				<h1 className="center-text header-lg">Players</h1>
 				<div className="row space-around">
-					{playerOne === null && (
+					{playerOne === null ? (
 						<PlayerInput
 							label="Player One"
 							onSubmit={(player) => setPlayerOne(player)}
 						/>
+					) : (
+						<PlayerPreview
+							username={playerOne}
+							label="Player One"
+							onReset={() => setPlayerOne(null)}
+						/>
 					)}
-					{playerTwo === null && (
+					{playerTwo === null ? (
 						<PlayerInput
 							label="Player Two"
 							onSubmit={(player) => setPlayerTwo(player)}
+						/>
+					) : (
+						<PlayerPreview
+							username={playerTwo}
+							label="Player Two"
+							onReset={() => setPlayerTwo(null)}
 						/>
 					)}
 				</div>
